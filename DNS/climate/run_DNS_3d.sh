e056563c5a067ca22f4e32cb48017fccc8383071
Tao Zhang <slnazhangtao@163.com>
2021-11-18 02:19:37 -0500

    Update run_DNS_3d.sh
    openmpi 4.0 should add the parameter "--mca  btl_openib_allow_ib 1" to support infiniband

--- DNS/climate/run_DNS_3d.sh
+++ DNS/climate/run_DNS_3d.sh
@@ -17,6 +17,6 @@
 ulimit -s unlimited
 
 cd /sdcc/u/tzhang/dns/DNS_install/cori/DNS/climate
-mpirun -n 64 /sdcc/u/tzhang/dns/DNS_install/cori/DNS/climate/climate -d 3 -p 4 4 4 -i input-pi-chamber/in-control-shorter -o out-control-shorter |& tee srun.log
+mpirun --mca  btl_openib_allow_ib 1 -n 64 /sdcc/u/tzhang/dns/DNS_install/cori/DNS/climate/climate -d 3 -p 4 4 4 -i input-pi-chamber/in-control-shorter -o out-control-shorter |& tee srun.log
 #srun -n 64 ./hello
 #cat /proc/cpuinfo
